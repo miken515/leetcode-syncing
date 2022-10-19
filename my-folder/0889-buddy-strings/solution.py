514010a844cfa5c319f5826095e714c874b174b1
miken515 <515.miken@gmail.com>
2022-10-19 04:14:05 +0000

    [LeetCode Sync] Runtime - 24 ms (5.26%), Memory - 14.4 MB (5.26%)

---
+++ my-folder/0889-buddy-strings/solution.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def buddyStrings(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        A = s
+        B = goal
+        if len(A) != len(B): return False
+        
+        # if strings are equal - check if there is a double to swap
+        if A == B:
+            return True if len(A) - len(set(A)) >= 1 else False
+        
+        # count differences between strings
+        diff = []
+        for i in range(len(A)):
+            if A[i] != B[i]:
+                diff.append(i)
+                if len(diff) > 2: return False
+                
+        # not exactly two differences
+        if len(diff) != 2: return False
+        
+        # check if can be swapped
+        if A[diff[0]] == B[diff[1]] and A[diff[1]] == B[diff[0]]:
+            return True
+        
+        return False
