dcfb693eee51a7cf119b9112557d5b34c45d1964
miken515 <515.miken@gmail.com>
2024-10-09 14:29:27 +0000

    [LeetCode Sync] Runtime - 48 ms (5.16%), Memory - 11.5 MB (100.00%)

--- my-folder/0009-palindrome-number/solution.py
+++ my-folder/0009-palindrome-number/solution.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        return str(x) == str(x)[::-1]
+        if x < 0:
+            return False
+
+        reversed_num = 0
+        temp = x
+
+        while temp != 0:
+            digit = temp % 10
+            reversed_num = reversed_num * 10 + digit
+            temp //= 10
+
+        return reversed_num == x
