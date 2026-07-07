f444687c42551dbbe035ea7a1cef101d728ec4ea
miken515 <515.miken@gmail.com>
2026-07-07 16:18:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.7 MB (29.82%)

---
+++ my-folder/0074-search-a-2d-matrix/solution.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            midRow = (top + bot) // 2
+            if target > matrix[midRow][-1]:
+                top = midRow + 1
+            elif target < matrix[midRow][0]:
+                bot = midRow - 1
+            else:
+                break
+
+        if not (top <= bot):
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
