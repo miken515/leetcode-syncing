1d7ea8ea4bcbb5935f5c015a03f541bd78d1d629
miken515 <515.miken@gmail.com>
2026-07-07 17:37:25 +0000

    [LeetCode Sync] Runtime - 203 ms (19.68%), Memory - 29.3 MB (7.39%)

---
+++ my-folder/0739-daily-temperatures/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        res = [0] * len(temperatures)
+        stack = [] # storing [index, temp]
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][1]:
+                stackIndex, stackTemp = stack.pop()
+                res[stackIndex] = (i - stackIndex)
+            stack.append([i, temp])
+        
+        return res
